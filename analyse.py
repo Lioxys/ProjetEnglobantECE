db387022626a8e89e1a89f52bf869c5d8c3a5fa4
Lioxys <ludkas.pilorge@gmail.com>
2026-02-23 10:54:23 +0100

    ajout dataset population code

--- analyse.py
+++ analyse.py
@@ -52,8 +52,8 @@ types_a_supprimer = [
     "Établissement d'enseignement supérieur"
 ]
 
-culture_df = culture_df[~culture_df['Type équipement ou lieu'].isin(types_a_supprimer)]
-culture_df['Type équipement ou lieu'].value_counts()
+culture_df = culture_df[~culture_df['Type_equipement_ou_lieu'].isin(types_a_supprimer)]
+culture_df['Type_equipement_ou_lieu'].value_counts()
 
 # Regroupement de certains types d'équipements ou lieux culturels similaires
 types_a_regrouper = [
@@ -63,11 +63,52 @@ types_a_regrouper = [
     'Centre culturel'
 ]
 
-culture_df.loc[:,'Type équipement ou lieu'] = culture_df['Type équipement ou lieu'].replace({
+culture_df.loc[:,'Type_equipement_ou_lieu'] = culture_df['Type_equipement_ou_lieu'].replace({
     'Centre de création artistique': 'Centre culturel',
     'Centre d\'art': 'Centre culturel',
     'Centre de création musicale': 'Centre culturel'
 })
 
-culture_df['Type équipement ou lieu'].value_counts()
+culture_df['Type_equipement_ou_lieu'].value_counts()
 
+# Traitement du fichier de population
+xls_population = pd.ExcelFile("population.xlsx")
+
+population_df = pd.read_excel(xls_population, sheet_name=xls_population.sheet_names[0], header=5) 
+population_df.shape[0]
+
+population_df[population_df.isnull().any(axis=1)]
+
+population_df[population_df["P18_POP"] < 0]
+
+columns_kept = ["LIBGEO","CODGEO","P18_POP", "P18_POP0014", "P18_POP1529", "P18_POP3044", "P18_POP4559", "P18_POP6074", "P18_POP7589", "P18_POP90P"]
+
+final_pop_df = population_df[columns_kept]
+
+# Fusion des deux DataFrames
+culture_df = culture_df.copy()
+
+# Villes en minuscules et sans espaces pour faciliter la fusion
+culture_df.loc[:, "ville_clean"] = culture_df["libelle_geographique"].str.strip().str.lower()
+
+final_pop_df = final_pop_df.copy()
+final_pop_df.loc[:, "ville_clean"] = final_pop_df["LIBGEO"].str.strip().str.lower()
+
+# Opérations pour uniformiser les noms de villes sur les départements
+culture_df.loc[culture_df["ville_clean"].str.startswith("paris"), "code_insee"] = "75000"
+culture_df.loc[culture_df["ville_clean"].str.startswith("marseille"), "code_insee"] = "13000"
+culture_df.loc[culture_df["ville_clean"].str.startswith("lyon"), "code_insee"] = "69000"
+
+
+final_pop_df.loc[final_pop_df["ville_clean"].str.startswith("paris"), "CODGEO"] = "75000"
+final_pop_df.loc[final_pop_df["ville_clean"].str.startswith("marseille"), "CODGEO"] = "13000"
+final_pop_df.loc[final_pop_df["ville_clean"].str.startswith("lyon"), "CODGEO"] = "69000"
+
+# Jointure 
+culturepop = pd.merge(
+    culture_df,
+    final_pop_df,
+    right_on=["CODGEO","ville_clean"],
+    left_on=["code_insee","ville_clean"],
+    how="inner"
+)
